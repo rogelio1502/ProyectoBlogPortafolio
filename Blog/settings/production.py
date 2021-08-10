351be9e54d9617d4b3eb377edcab9750732885fb
Yespinozah <rogelio15021515@gmail.com>
2021-08-10 13:23:22 -0500

    Se agregagron variables de entorno

--- Blog/settings/production.py
+++ Blog/settings/production.py
@@ -1,5 +1,4 @@
 from .base import *
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -9,4 +8,13 @@ ALLOWED_HOSTS = ['djangored.herokuapp.com']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = config("DATABASES")
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd59v3pqv2kf0tm',
+        'USER':'yxbotbxczvocdy',
+        'PASSWORD':'2a8c6dc3d312157ac1800b37ef830e7742c51a635434de16878d2e715893fdc8',
+        'HOST':'ec2-3-218-149-60.compute-1.amazonaws.com',
+        'PORT':'5432'
+    }
+}
